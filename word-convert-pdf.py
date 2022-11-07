fc2701cfc9fb02b244ea4809248c7b9a050f3a1a
CHONGANDXU <CqC33@outlook.com>
2022-11-07 12:12:17 +0800

    little try ( have bugs)

---
+++ word-convert-pdf.py
@@ -0,0 +1,28 @@
+from win32com.client import Dispatch
+from os import walk
+import os
+import time
+
+wdFormatPDF = 17
+
+def doc2pdf(input_file):
+    word = Dispatch('Word.Application')
+    doc = word.Documents.Open(input_file)
+    print(input_file)
+    doc.SaveAs(input_file.replace(".docx", ".pdf"), FileFormat=wdFormatPDF)
+    doc.Close()
+    word.Quit()
+
+
+if __name__ == "__main__":
+    doc_files = []
+    directory = "C:\\Users\\CqC33\\Downloads\\19"
+    for root, dirs, filenames in walk(directory):
+        for file in filenames:
+            if file.endswith(".doc") or file.endswith(".docx"):
+                print(os.path.abspath(root + "\\" + file))
+                print(os.path.abspath(root + "\\" + file).replace(".docx",".pdf"))
+                doc2pdf(os.path.abspath(root + "\\" + file))
+            else:
+                continue
+            time.sleep(3)
