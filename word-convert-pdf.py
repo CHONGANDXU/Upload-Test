6d9b0f9cf321d2869f80f5cc2943e3b2df1ab2cd
CHONGANDXU <CqC33@outlook.com>
2023-08-26 16:11:56 +0800

    remove privacy

--- word-convert-pdf.py
+++ word-convert-pdf.py
@@ -1,11 +1,13 @@
 from docx2pdf import convert
 import os
-import time
 
-word_path = r'/Users/chengchong/Desktop/OneDrive\ -\ yc3sq/docx'
+word_path = r'your word(.doc) file path'
 
-word_to_pdf = r'/Users/chengchong/Desktop/OneDrive\ -\ yc3sq/docx'
+word_to_pdf = r'your new pdf(.pdf) file path'
 
-for i,j,name in os.walk(word_path):
+for i, j, name in os.walk(word_path):
     for word_name in name:
-         convert(word_path+"/"+word_name, word_to_pdf+"/"+word_name.replace("docx","pdf"))
+        convert(
+            word_path + "/" + word_name,
+            word_to_pdf + "/" + word_name.replace("docx", "pdf"),
+        )
